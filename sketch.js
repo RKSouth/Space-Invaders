be336b6c69d569c75ad7516e42fbe592e1b79df2
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-05-19 14:39:39 -0700

    API IS WORKING

--- sketch.js
+++ sketch.js
@@ -1,91 +1,50 @@
-let canvasX= 1000;
-let canvasY= 1000;
+$(document).ready(function() {
+  $("#submitbtn").on("click", function(event) {
+    var search = $("input").val();
+    //it's pulling the array(full) and not the individual items from the array
+      event.preventDefault();
 
-let params = {
-
-  // height
-  height: 100,
-  heightMin: 10,
-  heightMax: 200,
- //width
-  width: 75,
-  widthMin:10,
-  widthMax:200,
-};
+    getStock(search);
+    // getForecast(search);
+    //   console.log(citiesArray);
+    //  addCities(search);
 
 
+    // localStorage.removeItem("search");
+    // localStorage.setItem("search",JSON.stringify(citiesArray));
+  
 
-// the gui object
-let gui;
-
-// let changeX = 75;
-// let changeY = 100;
-
-function setup() {
-  createCanvas(canvasX, canvasY);
-  gui = createGui('Number of Circles');
-  gui.addObject(params);
-}
+  });
 
 
+  function getStock (search){
+    
+    var queryURL = "https://cloud.iexapis.com/stable/stock/" + search + "/quote?token=pk_ab2f763af7f741d988ef13803ffd2ad8";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) { 
+      
+      console.log(response)
+      console.log(response.iexRealtimePrice)
+  // $(".city").empty();
 
-function draw() {
-  changeY = params.height;
-  changeX = params.width;
-stroke(126);
-// GRID GUIDE
-// line(500,0,500,50);
-// line(100,0,100,50);
-// line(900,0,900,50);
-// line(400,0,400,50);
-// line(600,0,600,50);
-
-let topCntrOne = 450;
-let topCntrTwo = 200;
-let topCntrThree = 650;
-let topCntrFour = 200;
-
-// TOP LINE
-// left line
-line(topCntrOne ,topCntrTwo ,topCntrOne - changeX, topCntrTwo + changeY);
-// center line
-line(topCntrOne,topCntrTwo,topCntrThree,topCntrFour);
-// right line
-line(topCntrThree,topCntrFour,topCntrThree + changeX , topCntrFour + changeY);
-
-// BOTTOM LINE
-let firstBtmY = 600;
-let secondBtmY = firstBtmY - changeY;
-// left line
-line(topCntrOne, topCntrTwo + firstBtmY, topCntrOne - changeX, topCntrTwo + secondBtmY);
-// center line
-line(topCntrOne, topCntrTwo + firstBtmY, topCntrThree, topCntrFour + firstBtmY);
-// right line
-line(topCntrThree, topCntrFour + firstBtmY, topCntrThree + changeX,  topCntrFour + secondBtmY);
-
-// LEFT LINE
-
-let leftCntrOne = 300;
-let leftCntrTwo = 400 ;
-let leftCntrThree = 300;
-let leftCntrFour = 600;
-
-// left line
-line(leftCntrOne,leftCntrTwo, leftCntrOne + changeX, leftCntrTwo - changeY);
-// center line
-line(leftCntrOne, leftCntrTwo, leftCntrThree, leftCntrFour);
-// // right line
-line(leftCntrThree, leftCntrFour,  leftCntrThree + changeX, leftCntrFour + changeY);
-
-let firstRightX = 500;
-let secondRightX = firstRightX-changeX; 
-
-// RIGHT LINE
-// left line
-line(leftCntrOne + firstRightX, leftCntrTwo, leftCntrOne + secondRightX, leftCntrTwo -changeY);
-// center line
-line( leftCntrOne + firstRightX, leftCntrTwo, leftCntrThree + firstRightX ,leftCntrFour);
-// // right line
-line(leftCntrThree + firstRightX, leftCntrFour ,leftCntrThree + secondRightX, leftCntrFour + changeY);
-
-}
+      // //create and add current days weather area
+      // var title =$("<h1>").text(response.name + " " +presentMonth + "/" + presentDay + "/" + presentYear).addClass("city-title");
+      // $(".city").append(title);
+      // var temperature = $("<p>").text("Temperature: " + response.main.temp);
+      // $(".city").append(temperature);
+      // var humidity = $("<p>").text("Humidity: " + response.main.humidity);
+      // $(".city").append(humidity);
+      // var windspd = $("<p>").text("Wind Spead: " + response.wind.speed);
+      // $(".city").append(windspd);
+      // var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+      // $(".city-title").append(img);
+      // // var UVIndex =getUVIndex(response.coord.lat, response.coord.lon);
+      // // $(".city").append(UVIndex);
+      // getUVIndex(response.coord.lat, response.coord.lon);
+    });
+    // $("#today").append();
+    
+  }
+})
