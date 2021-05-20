79993f6db87d17d67ae062111f3aac7bf31222a2
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-05-19 17:44:28 -0700

    renamed sketch to search

--- search.js
+++ search.js
@@ -40,6 +40,54 @@ $(document).ready(function() {
       $('.resultsTable').append( '<tr>' + '<td>' + 'changePercent' + '</td>' + '<td>' + response.changePercent + '</td>'+ '<td><button class="changePercent">' + 'Save Data' +'</button></td>' + '</tr>' );
       $('.resultsTable').append( '<tr>' + '<td>' + 'ytdChange' +  '</td>' + '<td>' + response.ytdChange +'</td>' + '<td><button class="ytdChange">' + 'Save Data' +'</button></td>' + '</tr>' );
 
+      $(".iexRealTimePrice").on("click", function(event) {   
+        const key = response.symbol + "'s " +"Real Time Price"
+        localStorage.setItem(key, response.iexRealtimePrice);
+        alert(response.symbol + "'s " +"Real Time Price was saved");
+      })
+
+      $(".previousClose").on("click", function(event) {
+        const key = response.symbol + "'s " +"Previous Close"
+        localStorage.setItem(key, response.previousClose);
+        alert(response.symbol + "'s " + "Previous close was saved");      
+
+      })
+
+      $(".week52High").on("click", function(event) {
+        const key = response.symbol + "'s " +"52 week High"
+        localStorage.setItem(key, response.week52High);
+        alert(response.symbol + "'s " + "52 week high was saved");      
+
+      })
+
+      $(".week52Low").on("click", function(event) {  
+        const key = response.symbol + "'s " +"52 week Low"
+        localStorage.setItem(key, response.week52Low);
+        alert(response.symbol + "'s " + "52 week low was saved");
+
+      })
+
+      $(".change").on("click", function(event) {
+        const key = response.symbol + "'s " +"change"
+        localStorage.setItem(key, response.change);
+        alert(response.symbol + "'s " + "change was saved");  
+
+      })
+
+      $(".changePercent").on("click", function(event) {      
+        const key = response.symbol + "'s " +"change percent"
+        localStorage.setItem( key, response.changePercent);
+        alert(response.symbol + "'s " + "change percent was saved");  
+
+      })
+
+      $(".ytdChange").on("click", function(event) {
+        const key = response.symbol + "'s " +"year-to-date change"
+        localStorage.setItem( key, response.change);
+        alert(response.symbol + "'s " + "year-to-date change was saved");        
+
+      })
+
     });
     // $("#today").append();
     
