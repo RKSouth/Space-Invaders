82b0ae15b28dcf7973d4d411e964592caacf5f0b
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-05-28 10:17:50 -0700

    Added Date and some comments

--- saved.js
+++ saved.js
@@ -1,53 +1,80 @@
 $(document).ready(function () {
+    // set local storage to a variable
     local = localStorage;
 
     let keyNames = [];
 
+    // getting things from storage
     allStorage();
-    for (var name in local) {
-        console.log(name);
 
+    // today's date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    // document.write(today);
+    // $(".theDate").append(today)
+
+    
+
+    // getting the key names for each item
+    for (var name in local) {
+        // we need to remove the names we don't want to display
         if (name === "length" || name === "clear" || name === "getItem" || name === "key" || name === "removeItem" || name === "setItem") {
         } else {
             keyNames.push(name);
         }
     }
-
+    // creating remove button for each item
     keyNames.map(name => (
         $('#remove').append(
-            '<p><button id="'+ name +'">' + "Remove" + '</button></p>'
+            '<p><button id="' + name + '">' + "Remove" + '</button></p>'
         )
     ))
-    
-    $("button").click(function() {
+    // deleting items from storage
+    $("button").click(function () {
         console.log(this.id);
         localStorage.removeItem(this.id);
         location.reload();
     });
 
     
-
-    console.log(keyNames[2].indexOf("'s "));
-    console.log(keyNames[2].substring( (keyNames[2].indexOf("'s ") + 3) ))
+    console.log(keyNames[2].indexOf("/"));
+    console.log(keyNames[2].substring((keyNames[2].indexOf("'s ") + 3), (keyNames[2].indexOf("/") - 2)))
+    console.log(keyNames[2].substring((keyNames[2].indexOf("/") - 2)))
     console.log(keyNames)
-    
-    
+
+    let theDate = [];
+
+    for (i = 0; i < keyNames.length; i++) {
+        theDate.push(keyNames[i].substring((keyNames[i].indexOf("/") - 2)))
+        
+    }
+    console.log(theDate)
+    theDate.map(item => (
+        $("#date").append(
+            '<li><p>' + item + '</p></li>'
+        )
+    ))
+
 
     finalNames = [];
-    for (i=0; i<keyNames.length; i++){
-        finalNames.push(keyNames[i].substring( (keyNames[i].indexOf("'s ") + 3) ))
+    for (i = 0; i < keyNames.length; i++) {
+        // removing the symbol and possessive from key name and date to get the description 
+        finalNames.push(keyNames[i].substring((keyNames[i].indexOf("'s ") + 3), (keyNames[i].indexOf("/") - 2)))
     }
     console.log(finalNames);
     finalNames.map(item => (
         $('#name').append(
-            '<p>' + item + '</p>'
+            '<p  class="name" id="' + item + '">' + item + '</p>'
         )
 
 
     ));
     function allStorage() {
-
-
+        // create values array
         var values = [],
             keys = Object.keys(localStorage),
             i = keys.length;
@@ -63,21 +90,22 @@ $(document).ready(function () {
         // $('#savedResults').append(  '<table>' );
         values.map(number => (
             $('#results').append(
-                '<p>' + number + '</p>' 
+                '<p>' + number + '</p>'
             )
         ));
     }
     symbol = []
     for (i = 0; i < keyNames.length; i++) {
-        // console.log(keyNames[i]);
-        // console.log(keyNames[i].indexOf("'s "));
-        symbol.push(keyNames[i].slice(0,keyNames[i].indexOf("'s ")));
+        // pushing symbol name to symbol array, making sure to remove possessives and spacing
+        symbol.push(keyNames[i].slice(0, keyNames[i].indexOf("'s ")));
     }
+    // map and display array to a list
     symbol.map(stock =>
         $('#stock').append(
-            '<p>' + stock + '</p>'
+            '<li><p id="' + stock + '">' + stock + '</p></li>'
         )
     )
 
     console.log(symbol);
+
 })
--- search.js
+++ search.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $("#submit").on("click", function(event) {
+  $("#submit").on("click", function (event) {
 
     let resultsTable = $(".resultsTable");
-  
+
     let str = "";
-    for (let i=0; i < resultsTable.length; i++) {
+    for (let i = 0; i < resultsTable.length; i++) {
       resultsTable[i].classList.remove("active");
     }
-  
+
     this.classList.add("active");
     console.log($(".companySymbol"));
 
@@ -24,85 +24,95 @@ $(document).ready(function() {
 
     const search = $("input").val();
     //it's pulling the array(full) and not the individual items from the array
-       event.preventDefault();
+    event.preventDefault();
     // $(".searchResults").show();
     getStock(search);
-  
+
 
   });
 
+  // today's date
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  var hh = today.getHours();
+  var min = String(today.getMinutes() + 1).padStart(2, '0');
 
-  function getStock (search){
-    
+  today = mm + '/' + dd + '/' + yyyy + ' ' + hh + ":" + min;
+  // document.write(today);
+  $(".theDate").append(today)
+
+  function getStock(search) {
+
     var queryURL = "https://cloud.iexapis.com/stable/stock/" + search + "/quote?token=pk_ab2f763af7f741d988ef13803ffd2ad8";
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) { 
+    }).then(function (response) {
       console.log(response)
       $(".companyName").append(response.companyName);
       $(".companySymbol").append(response.symbol);
       $(".primaryExchange").append(response.primaryExchange);
 
 
-      $('.resultsTable').append( '<tr>' + '<td>' + 'RealtimePrice' +'</td>' + '<td>' + response.iexRealtimePrice + '</td>' + '<td><button class="iexRealTimePrice">' + 'Save Data' +'</button></td>'+ '</tr>' );
-      $('.resultsTable').append( '<tr>' + '<td>' + 'previous close' + '</td>' + '<td>'  + response.previousClose + '</td>' + '<td><button class="previousClose">' + 'Save Data' +'</button></td>'+ '</tr>' );
-      $('.resultsTable').append( '<tr>' + '<td>' + 'week52High' + '</td>' + '<td>'  + response.week52High + '</td>' + '<td><button class="week52High">' + 'Save Data' +'</button></td>'+ '</tr>' );
-      $('.resultsTable').append( '<tr>' + '<td>' + 'week52Low' + '</td>' + '<td>'  + response.week52Low + '</td>' + '<td><button class="week52Low">' + 'Save Data' +'</button></td>' + '</tr>' );
-      $('.resultsTable').append( '<tr>' + '<td>' + 'change' + '</td>' + '<td>'  + response.change + '</td>'+ '<td><button class="change">' + 'Save Data' +'</button></td>' +  '</tr>' );
-      $('.resultsTable').append( '<tr>' + '<td>' + 'changePercent' + '</td>' + '<td>' + response.changePercent + '</td>'+ '<td><button class="changePercent">' + 'Save Data' +'</button></td>' + '</tr>' );
-      $('.resultsTable').append( '<tr>' + '<td>' + 'ytdChange' +  '</td>' + '<td>' + response.ytdChange +'</td>' + '<td><button class="ytdChange">' + 'Save Data' +'</button></td>' + '</tr>' );
+      $('.resultsTable').append('<tr>' + '<td>' + 'Real Time Price' + '</td>' + '<td>' + response.iexRealtimePrice + '</td>' + '<td><button class="iexRealTimePrice">' + 'Save Data' + '</button></td>' + '</tr>');
+      $('.resultsTable').append('<tr>' + '<td>' + 'Previous Close' + '</td>' + '<td>' + response.previousClose + '</td>' + '<td><button class="previousClose">' + 'Save Data' + '</button></td>' + '</tr>');
+      $('.resultsTable').append('<tr>' + '<td>' + '52 week High' + '</td>' + '<td>' + response.week52High + '</td>' + '<td><button class="week52High">' + 'Save Data' + '</button></td>' + '</tr>');
+      $('.resultsTable').append('<tr>' + '<td>' + '52 week Low' + '</td>' + '<td>' + response.week52Low + '</td>' + '<td><button class="week52Low">' + 'Save Data' + '</button></td>' + '</tr>');
+      $('.resultsTable').append('<tr>' + '<td>' + 'Change' + '</td>' + '<td>' + response.change + '</td>' + '<td><button class="change">' + 'Save Data' + '</button></td>' + '</tr>');
+      $('.resultsTable').append('<tr>' + '<td>' + 'Change Percent' + '</td>' + '<td>' + response.changePercent + '</td>' + '<td><button class="changePercent">' + 'Save Data' + '</button></td>' + '</tr>');
+      $('.resultsTable').append('<tr>' + '<td>' + 'ytd Change' + '</td>' + '<td>' + response.ytdChange + '</td>' + '<td><button class="ytdChange">' + 'Save Data' + '</button></td>' + '</tr>');
 
-      $(".iexRealTimePrice").on("click", function(event) {   
-        const key = response.symbol + "'s " +"Real Time Price"
+      $(".iexRealTimePrice").on("click", function (event) {
+        const key = response.symbol + "'s " + "Real Time Price" + today
         localStorage.setItem(key, response.iexRealtimePrice);
-        alert(response.symbol + "'s " +"Real Time Price was saved");
+        alert(response.symbol + "'s " + "Real Time Price was saved");
       })
 
-      $(".previousClose").on("click", function(event) {
-        const key = response.symbol + "'s " +"Previous Close"
+      $(".previousClose").on("click", function (event) {
+        const key = response.symbol + "'s " + "Previous Close" + today
         localStorage.setItem(key, response.previousClose);
-        alert(response.symbol + "'s " + "Previous close was saved");      
+        alert(response.symbol + "'s " + "Previous close was saved");
 
       })
 
-      $(".week52High").on("click", function(event) {
-        const key = response.symbol + "'s " +"52 week High"
+      $(".week52High").on("click", function (event) {
+        const key = response.symbol + "'s " + "52 week High" + today
         localStorage.setItem(key, response.week52High);
-        alert(response.symbol + "'s " + "52 week high was saved");      
+        alert(response.symbol + "'s " + "52 week high was saved");
 
       })
 
-      $(".week52Low").on("click", function(event) {  
-        const key = response.symbol + "'s " +"52 week Low"
+      $(".week52Low").on("click", function (event) {
+        const key = response.symbol + "'s " + "52 week Low" + today
         localStorage.setItem(key, response.week52Low);
         alert(response.symbol + "'s " + "52 week low was saved");
 
       })
 
-      $(".change").on("click", function(event) {
-        const key = response.symbol + "'s " +"change"
+      $(".change").on("click", function (event) {
+        const key = response.symbol + "'s " + "change" + today
         localStorage.setItem(key, response.change);
-        alert(response.symbol + "'s " + "change was saved");  
+        alert(response.symbol + "'s " + "change was saved");
 
       })
 
-      $(".changePercent").on("click", function(event) {      
-        const key = response.symbol + "'s " +"change percent"
-        localStorage.setItem( key, response.changePercent);
-        alert(response.symbol + "'s " + "change percent was saved");  
+      $(".changePercent").on("click", function (event) {
+        const key = response.symbol + "'s " + "change percent" + today
+        localStorage.setItem(key, response.changePercent);
+        alert(response.symbol + "'s " + "change percent was saved");
 
       })
 
-      $(".ytdChange").on("click", function(event) {
-        const key = response.symbol + "'s " +"year-to-date change"
-        localStorage.setItem( key, response.change);
-        alert(response.symbol + "'s " + "year-to-date change was saved");        
+      $(".ytdChange").on("click", function (event) {
+        const key = response.symbol + "'s " + "year-to-date change" + today
+        localStorage.setItem(key, response.change);
+        alert(response.symbol + "'s " + "year-to-date change was saved");
 
       })
 
     });
-    // $("#today").append();
-    
+
   }
 })
