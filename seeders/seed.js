581851839cff21cc809b0226a4be153a13b372ed
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-07-13 17:42:40 -0700

    began connecting to the db

---
+++ seeders/seed.js
@@ -0,0 +1,30 @@
+let mongoose = require("mongoose");
+let db = require("../models");
+
+
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    mongoAtlasUri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(" Mongoose is connected"),
+  );
+} catch (e) {
+  console.log("could not connect");
+}
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
+
+
+db.Stock.deleteMany({})
+  .then(() => db.Stock.collection.insertMany(stockSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
